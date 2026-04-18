5225314376d214b61b136524b59697db9d1dcd99
Misbah Ullah <misbahu094@gmail.com>
2026-04-18 13:06:34 +0500

    Added Room Log Table into 01_warden_schema.sql

--- database/01_warden_schema.sql
+++ database/01_warden_schema.sql
@@ -8,12 +8,29 @@ CREATE SCHEMA IF NOT EXISTS warden;
 REVOKE ALL ON SCHEMA warden FROM PUBLIC;
 GRANT ALL ON SCHEMA warden TO warden;
 
+-- ===================================================
+
 CREATE TABLE IF NOT EXISTS warden.players (
 	player_id 		SERIAL PRIMARY KEY,
 	username 		VARCHAR(50) UNIQUE NOT NULL,
-	created_at 		TIMESTAMPTZ DEFAULT NOW(),
-	completed_at 	TIMESTAMPTZ,
+	created_at 		TIMESTAMPTZ DEFAULT NOW(),		-- TIMESTAMP TZ is timestamp with timezone
+	completed_at 	TIMESTAMPTZ,					-- NULL unitl they successfully escape
 	current_room 	VARCHAR(50) DEFAULT 'lobby',
 	is_active 		BOOLEAN DEFAULT TRUE
 );
+
+-- ===================================================
+
+CREATE TABLE IF NOT EXISTS warden.room_log(
+	log_id 		SERIAL PRIMARY KEY,
+	player_id 	INT NOT NULL REFERENCES warden.players(player_id),	--Foerign Key
+	room_name 	VARCHAR(50) NOT NULL,
+	solved_at 	TIMESTAMPTZ DEFAULT NOW(),
+	attempts 	INT DEFAULT 1,
+	time_taken 	INTERVAL		-- Example: '00:12:23' (Hours:minute:seconds)
+);
+
+-- ===================================================
+
+
 
